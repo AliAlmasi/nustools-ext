34b86543564f0a2c98f4697a7e77e06762488a69
Ali Almasi <a710almasi@gmail.com>
2025-10-13 23:24:57 +0330

    feat: init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NUSTools Browser Extension
+
+This repository contains source code of the [NUSTools.ir](https://nustools.ir) browser extension.
+
+WORK IN PROGRESS.
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import path from "path";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const rootPath = (address = "") =>
+  address ? path.resolve(__dirname, address.trim()) : __dirname;
+
+export default defineConfig(({ mode }) => {
+  const isMv3 = mode === "mv3";
+
+  const manifestFile = isMv3
+    ? "public/manifest.v3.json"
+    : "public/manifest.v2.json";
+
+  const outDir = isMv3 ? "dist/mv3" : "dist/mv2";
+
+  return {
+    publicDir: false,
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          { src: manifestFile, dest: ".", rename: "manifest.json" },
+          { src: "public/icons", dest: "." },
+        ],
+      }),
+    ],
+    build: {
+      outDir,
+      rollupOptions: {
+        input: {
+          background: rootPath("src/background.js"),
+          content: rootPath("src/content.js"),
+        },
+        output: {
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "background") return "background.js";
+            if (chunkInfo.name === "content") return "content.js";
+            return "assets/[name]-[hash].js";
+          },
+        },
+      },
+    },
+  };
+});
