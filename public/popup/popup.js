366dbf65f91f7720be8331763b29243828cc4fc7
Ali Almasi <a710almasi@gmail.com>
2026-02-01 13:11:31 +0330

    fix: updated popup script

--- public/popup/popup.js
+++ public/popup/popup.js
@@ -1,34 +1,36 @@
-function newtab(href) {
-  const a = document.createElement("a");
-  a.href = href;
-  a.setAttribute("target", "_blank");
-  a.click();
-  a.remove();
-}
+// function newtab(href) {
+//   const a = document.createElement("a");
+//   a.href = href;
+//   a.setAttribute("target", "_blank");
+//   a.click();
+//   a.remove();
+// }
+
+document.addEventListener("contextmenu", (e) => e.preventDefault());
 
 const currentVersion = chrome.runtime.getManifest().version;
 
 document.addEventListener(
   "DOMContentLoaded",
-  () => (document.getElementById("version").textContent = currentVersion)
+  () => (document.getElementById("version").textContent = currentVersion),
 );
 
-const updateButton = document.getElementById("update");
-const showLatestVersionMessage = () => {
-  const oldTextContent = updateButton.textContent;
-  updateButton.textContent = "نیازی نیست :)";
-  updateButton.setAttribute("disabled", "true");
-  console.log("clicked");
+// const updateButton = document.getElementById("update");
 
-  setTimeout(() => {
-    updateButton.textContent = oldTextContent;
-    updateButton.removeAttribute("disabled");
-  }, 2 * 1000);
-};
+// const showLatestVersionMessage = () => {
+//   const oldTextContent = updateButton.textContent;
+//   updateButton.textContent = "نیازی نیست :)";
+//   updateButton.setAttribute("disabled", "true");
+//   console.log("clicked");
 
-document.addEventListener("contextmenu", (e) => e.preventDefault());
-updateButton.addEventListener("click", () => {
-  const latestVersion = "v0.0.7"; // fetch from github latest tag
-  if (currentVersion === latestVersion) showLatestVersionMessage();
-  else newtab("https://github.com/alialmasi/nustools-ext/releases/latest");
-});
+//   setTimeout(() => {
+//     updateButton.textContent = oldTextContent;
+//     updateButton.removeAttribute("disabled");
+//   }, 2 * 1000);
+// };
+
+// updateButton.addEventListener("click", () => {
+//   const latestVersion = "v0.0.7"; // fetch from github latest tag
+//   if (currentVersion === latestVersion) showLatestVersionMessage();
+//   else newtab("https://github.com/alialmasi/nustools-ext/releases/latest");
+// });
